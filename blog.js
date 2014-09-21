e196bab1e35d9d8fa515b5ce2cfa42ba3a997be7
namho102 <namhoang.td09@gmail.com>
2014-09-21 17:56:05 +0700

    blog.js

---
+++ blog.js
@@ -0,0 +1,14 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("bBQvikwcUgKJgPj0WFcc6tHOBeTT8ire8jF3xFmn", "4LMnOtk2Ga4h7Fl2lX7qRWsxjCBFtjgMX3ep03lx");
+ 
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.save({foo: "bar"}).then(function(object) {
+      alert("yay! it worked");
+    });
+ 
+});
